0425d4add52e3df981e371e23ba57272ffd17f2a
Rishav78 <rishavgarg789@gmail.com>
2019-07-20 09:46:50 +0530

    commit

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-// (function(){
+(function(){
     let span = document.querySelector('span');
-    span.innerHTML = 0;
 
     let canvas = document.querySelector('canvas');
+    canvas.width = 450, canvas.height = 450;
     let c = canvas.getContext('2d');
     let pause=false;
     let gameOver = false;
-    let stop = false;
     let speed = Date.now();
 
 
     let score = 0;
-    canvas.width = 450, canvas.height = 450;
     let dimentions = 30;
 
 
@@ -21,11 +19,8 @@
 
 
 
-
-
-
+    span.innerHTML = 0;
     board.createBoard();
-    board.drawBoard();
     shape.newBlock()
     animate();
 
@@ -33,14 +28,14 @@
     function animate() {
         let speed2 = Date.now();
         if(!gameOver)
-             requestAnimationFrame(this.animate);
+             requestAnimationFrame(animate);
         if(speed2 - speed > 700 && !pause){
             speed = speed2;
             if(!shape.move(0,1,0)) {
                 board.lockState(shape.activeShape,shape.activeShapeIndex);
                 board.destroyRow(shape.activeShape);
-                undo=[];
-                redo=[];
+                shape.undo.reset();
+                shape.redo.reset();
                 shape.newBlock();
             }
         }
@@ -61,33 +56,12 @@
         this.reset = function() {
             this.array = [];
         }
-    }
-    
-    
-    
-    function redoStep(){
-        if(redo.length && redo[redo.length - 1].type === 1){
-            console.log('yuioiju')
-            move(redo[redo.length - 1].move[0], redo[redo.length - 1].move[1], 1);
-            speed = Date.now();
-            redo.splice(redo.length - 1,1)
-        }
-    }
-    function undoStep(){
-        if(undo.length && undo[undo.length - 1].type === 1){
-            move(undo[undo.length - 1].move[0], undo[undo.length - 1].move[1], 1);
-            speed = Date.now();
-            let removed = undo.splice(undo.length - 1,1)[0];
-            removed.move[0] *= -1;
-            removed.move[1] *= -1;
-            redo.push(removed);
+        this.isEmpty = function() {
+            return this.array.length === 0;
         }
     }
     
     
-    
-    
-    // newBlock();
     
     window.addEventListener('keydown', (e) => {
         if(!gameOver){
@@ -114,18 +88,18 @@
                 case 40: //down
                     speed = Date.now();
                     if(!shape.move(0,1,0)) {
-                        // undo=[];
-                        // redo=[];
+                        shape.undo.reset();
+                        shape.redo.reset();
                         board.lockState(shape.activeShape,shape.activeShapeIndex);
                         board.destroyRow(shape.activeShape);
                         shape.newBlock();
                     }
                     break;
                 case 85:
-                    undoStep();
+                    shape.undoStep();
                     break;
                 case 82:
-                    redoStep();
+                    shape.redoStep();
                 default:
                     console.log(e.keyCode)
             }
@@ -147,6 +121,7 @@
             for(let i=0;i<this.row;i++){
                 this.board[i] = new Array(this.colums).fill(-1);
             }
+            this.drawBoard();
         }
 
         this.drawBoard = function() {
@@ -201,7 +176,6 @@
             for(let i=0;i<array.length;i++){
                 for(let j=0;j<array.length;j++){
                     if(array[i][j] > 0){
-                        // console.log('ghj')
                         drawer.drawSquare(j+shape.x,i+shape.y, color ? color : board.backgroundColor);
                     }
                 }
@@ -260,12 +234,14 @@
                 [0, 0, 0]
             ],
         ];
+        this.undo = new Stack();
+        this.redo = new Stack();
         this.availableShapeColor = ['#004d00','#4d004d','#b30000','#0000ff','#0d0d0d','#000080', '#333300'];
         this.activeShape = null;
         this.activeShapeIndex = null;
         this.x = null;
         this.y = null;
-
+        
         this.newBlock = function() {
             // 
             this.activeShapeIndex = (Math.floor(Math.random()*10))%this.availableShapes.length;;
@@ -281,14 +257,14 @@
                 this.x += x;
                 this.y += y;
                 if(!store){
-                    // if(redo.length){
-                    //     // redo = [];
-                    // }
-                    // undo.push({
-                    //     type: 1,
-                    //     shape: this.activeShapeIndex,
-                    //     move: [-x, -y],
-                    // })
+                    if(!this.redo.isEmpty()){
+                        this.redo.reset();
+                    }
+                    this.undo.push({
+                        type: 1,
+                        shape: this.activeShapeIndex,
+                        move: [-x, -y],
+                    })
                 }
             }
             drawer.draw(this.activeShape);
@@ -306,11 +282,11 @@
                 };
             };
             if(!shape.collision(newArray,0,0)){
-                // undo.push({
-                //     type: 0,
-                //     shape: shapeIndex,
-                //     dir: (dir+1)%2,
-                // });
+                this.undo.push({
+                    type: 0,
+                    shape: this.activeShapeIndex,
+                    dir: (dir+1)%2,
+                });
                 this.activeShape = newArray;
             }
         }
@@ -338,10 +314,33 @@
             }
             return false;
         }
+
+        this.redoStep = function() {
+            if(!this.redo.isEmpty()){
+                let step = this.redo.pop();
+                if(step.type === 1){
+                    shape.move(step.move[0], step.move[1], 1);
+                    speed = Date.now();
+                }
+            }
+        }
+
+        this.undoStep = function() {
+            if(!this.undo.isEmpty()){
+                let step = this.undo.pop();
+                if(step.type === 1){    
+                    shape.move(step.move[0], step.move[1], 1);
+                    speed = Date.now();
+                    step.move[0] *= -1;
+                    step.move[1] *= -1;
+                    this.redo.push(step);
+                }
+            }
+        }
     }
 
     
     //-----------------------------------------------------------------------------------------------------------------------------------//
 
 
-// })();
+})();
